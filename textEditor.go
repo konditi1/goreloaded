651f12638b2a92de95aa27fcd9c35f267f3b8e21
konditi1 <konditi1@gmail.com>
2024-04-12 20:36:24 +0300

    Implemented regex

---
+++ textEditor.go
@@ -0,0 +1,59 @@
+package goreloaded
+
+import (
+	"strings"
+	"unicode"
+)
+
+func TextEditor(str string) string {
+	strArr := strings.Split(string(str), " ")
+
+	str = Vowel(strArr)
+
+	for _, v := range str {
+		if unicode.IsPunct(v) {
+			str = Punctuation(str)
+		}
+	}
+
+	str = Regex2(str)
+	strArr = strings.Fields(str)
+
+	for _, v := range strArr {
+		if v == "(up)" || v == "(up)," || v == "(up)." || v == "(up)!" || v == "(up)?" {
+			str = Upper(strArr)
+			strArr = strings.Fields(str)
+		} else if v == "(up," {
+			str = IndexUpper(strArr)
+			strArr = strings.Fields(str)
+		} else if v == "(low)" || v == "(low)," || v == "(low)." || v == "(low)!" || v == "(low)?" {
+			str = Lower(strArr)
+			strArr = strings.Fields(str)
+		} else if v == "(low," {
+			str = IndexLow(strArr)
+			strArr = strings.Fields(str)
+		} else if v == "(cap)" || v == "(cap)," || v == "(cap)." || v == "(cap)!" || v == "(cap)?" {
+			str = Capitalize(strArr)
+			strArr = strings.Fields(str)
+		} else if v == "(cap," {
+			str = IndexCap(strArr)
+			strArr = strings.Fields(str)
+		} else if v == "(hex)" || v == "(hex)," || v == "(hex)." || v == "(hex)!" || v == "(hex)?" {
+			str = HexaDeci(strArr)
+			strArr = strings.Fields(str)
+		} else if v == "(bin)" || v == "(bin)," || v == "(bin)." || v == "(bin)!" || v == "(bin)?" {
+			str = Binary(strArr)
+			strArr = strings.Fields(str)
+		}
+	}
+
+	for _, v := range str {
+		if unicode.IsPunct(v) {
+			str = Punctuation(str)
+		}
+	}
+
+	regexp := Regex(str)
+
+	return regexp
+}
--- textEditor/main.go
+++ textEditor/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
-	"unicode"
 
 	"goreloaded"
 )
@@ -17,59 +15,12 @@ func main() {
 	// destFile := os.Args[2]
 
 	file, err := os.ReadFile(sourceFile)
-
 	if err != nil {
 		fmt.Println("Error! Could not read file", err)
 	}
+
 	str := string(file)
-	strArr := strings.Split(string(str), " ")
-
-	str = goreloaded.Vowel(strArr)
-
-	for _, v := range str {
-		if unicode.IsPunct(v) {
-			str = goreloaded.Punctuation(str)
-		}
-	}
-
-	str = goreloaded.Regex2(str)
-	strArr = strings.Fields(str)
-
-	for _, v := range strArr {
-		if v == "(up)" || v == "(up)," || v == "(up)." || v == "(up)!" || v == "(up)?" {
-			str = goreloaded.Upper(strArr)
-			strArr = strings.Fields(str)
-		} else if v == "(up," {
-			str = goreloaded.IndexUpper(strArr)
-			strArr = strings.Fields(str)
-		} else if v == "(low)" || v == "(low)," || v == "(low)." || v == "(low)!" || v == "(low)?" {
-			str = goreloaded.Lower(strArr)
-			strArr = strings.Fields(str)
-		} else if v == "(low," {
-			str = goreloaded.IndexLow(strArr)
-			strArr = strings.Fields(str)
-		} else if v == "(cap)" || v == "(cap)," || v == "(cap)." || v == "(cap)!" || v == "(cap)?" {
-			str = goreloaded.Capitalize(strArr)
-			strArr = strings.Fields(str)
-		} else if v == "(cap," {
-			str = goreloaded.IndexCap(strArr)
-			strArr = strings.Fields(str)
-		} else if v == "(hex)" || v == "(hex)," || v == "(hex)." || v == "(hex)!" || v == "(hex)?" {
-			str = goreloaded.HexaDeci(strArr)
-			strArr = strings.Fields(str)
-		} else if v == "(bin)" || v == "(bin)," || v == "(bin)." || v == "(bin)!" || v == "(bin)?" {
-			str = goreloaded.Binary(strArr)
-			strArr = strings.Fields(str)
-		}
-	}
-
+	editedText := goreloaded.TextEditor(str)
+	fmt.Println(editedText)
 	
-	for _, v := range str {
-		if unicode.IsPunct(v) {
-			str = goreloaded.Punctuation(str)
-		}
-	}
-
-	regexp := goreloaded.Regex(str)
-	fmt.Println(regexp)
 }
